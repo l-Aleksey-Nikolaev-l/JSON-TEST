d7a85d8d7f393e6fa461b51699b462db42f5ff95
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-08-22 14:04:52 +0200

    JSON-SERVER V1.4

--- api/server.js
+++ api/server.js
@@ -11,8 +11,14 @@ server.use(jsonServer.rewriter({
 }))
 server.use(router)
 
-var cors = require('cors');
-app.use(cors());
+router.get('/', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    res.send('cors problem fixed:)');
+});
 
 server.listen(3000, () => {
     console.log('JSON Server is running')
