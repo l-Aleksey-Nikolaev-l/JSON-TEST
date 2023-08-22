c3d42a77910899be915e54a4c05d80c473ebe3d9
l-Aleksey-Nikolaev-l <senatorfirst1@gmail.com>
2023-08-22 14:55:04 +0200

    JSON-SERVER V1.6

--- api/server.js
+++ api/server.js
@@ -3,39 +3,6 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
-
-
-var express = require('express');
-var app = express();
-
-
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
-
-
-
-
-
 server.use(middlewares)
 
 server.use(jsonServer.rewriter({
